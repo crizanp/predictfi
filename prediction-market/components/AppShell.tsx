65bd18a776b3b3c823143062f1ded080066f3a7e
crijanp <srij89995@gmail.com>
2026-05-28 15:01:59 +0545

    whitepaer statuic uodated

---
+++ prediction-market/components/AppShell.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import type { ReactNode } from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+import GlobalBanner from './GlobalBanner'
+import TopAnnouncement from './TopAnnouncement'
+import CornerNotifications from './CornerNotifications'
+
+const DOC_ROUTE_PREFIXES = ['/whitepaper', '/pitchdeck', '/tokonomics', '/roadmap']
+
+function isDocsRoute(pathname: string): boolean {
+  return DOC_ROUTE_PREFIXES.some((prefix) => pathname.startsWith(prefix)) || pathname.endsWith('.html')
+}
+
+export default function AppShell({ children }: { children: ReactNode }) {
+  const pathname = usePathname()
+
+  if (isDocsRoute(pathname)) {
+    return <>{children}</>
+  }
+
+  return (
+    <>
+      <TopAnnouncement />
+      <Sidebar />
+      <div className="appMain">
+        <Navbar />
+        <GlobalBanner />
+        {children}
+      </div>
+      <CornerNotifications />
+    </>
+  )
+}
